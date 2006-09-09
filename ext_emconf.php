41a9c53903a0c8fec3f348cc8776648e74b31d7a
stucki <stucki@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-09-09 12:12:30 +0000

    Initial import based on static_info_tables 2.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@3698 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "static_info_tables"
+#
+# Auto generated 29-06-2006 00:47
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static Info Tables',
+	'description' => 'Some database tables with usefull informations about countries, languages and currencies. Still lacks some data but the overall concept is stable.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '2.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Rene Fritz',
+	'author_email' => 'r.fritz@colorcube.de',
+	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:19:{s:20:"class.ext_update.php";s:4:"fea5";s:33:"class.tx_staticinfotables_div.php";s:4:"d183";s:38:"class.tx_staticinfotables_encoding.php";s:4:"06e9";s:41:"class.tx_staticinfotables_syslanguage.php";s:4:"abba";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"e9ff";s:14:"ext_tables.php";s:4:"f1d8";s:14:"ext_tables.sql";s:4:"1c58";s:25:"ext_tables_static+adt.sql";s:4:"431d";s:25:"icon_static_countries.gif";s:4:"2a46";s:26:"icon_static_currencies.gif";s:4:"a1e2";s:25:"icon_static_languages.gif";s:4:"639f";s:27:"icon_static_territories.gif";s:4:"aab5";s:16:"locallang_db.xml";s:4:"0063";s:7:"tca.php";s:4:"6f6a";s:14:"doc/manual.sxw";s:4:"bf19";s:37:"pi1/class.tx_staticinfotables_pi1.php";s:4:"04b7";s:39:"static/static_info_tables/constants.txt";s:4:"aaf6";s:35:"static/static_info_tables/setup.txt";s:4:"82b7";}',
+);
+
+?>
