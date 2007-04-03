020ac05bbea7a31244d7f95f72cffc94f68b9c45
dbruehlmeier <dbruehlmeier@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-04-03 17:04:02 +0000

    Added new table 'static_markets' (regulated by ISO 10383)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@5275 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -124,6 +124,24 @@ CREATE TABLE static_taxes (
   KEY parent (pid)
 );
 
+
+#
+# Table structure for table "static_markets"
+#
+CREATE TABLE static_markets (
+  uid int(11) auto_increment,
+  pid int(11) default '0',
+  country int(11) default '0',
+  mic varchar(4) default '',
+  institution_description varchar(90) default '',
+  acronym varchar(30) default '',
+  city varchar(30) default '',
+  url varchar(90) default '',
+  PRIMARY KEY (uid),
+  KEY parent (pid)
+);
+
+
 #
 # Table structure for table "tx_staticinfotables_hotlist"
 #
--- tca.php
+++ tca.php
@@ -803,4 +803,79 @@ $TCA['static_taxes'] = Array (
 	)
 );
 
+$TCA['static_markets'] = array (
+	'ctrl' => $TCA['static_markets']['ctrl'],
+	'interface' => array (
+		'showRecordFieldList' => 'country,mic,institution_description,acronym,city,url'
+	),
+	'feInterface' => $TCA['tx_staticmarkets_markets']['feInterface'],
+	'columns' => array (
+		'country' => Array (		
+			'exclude' => 1,		
+			'label' => 'LLL:EXT:static_info_tables/locallang_db.xml:static_markets.country',
+			'config' => Array (
+				'type' => 'select',	
+				'foreign_table' => 'static_countries',	
+				'foreign_table_where' => 'AND static_countries.pid=###SITEROOT### ORDER BY static_countries.uid',	
+				'size' => 1,	
+				'minitems' => 0,
+				'maxitems' => 1,
+			)
+		),
+		'mic' => Array (		
+			'exclude' => 1,		
+			'label' => 'LLL:EXT:static_info_tables/locallang_db.xml:static_markets.mic',
+			'config' => Array (
+				'type' => 'input',	
+				'size' => '5',	
+				'max' => '4',	
+				'eval' => 'trim',
+			)
+		),
+		'institution_description' => Array (		
+			'exclude' => 1,		
+			'label' => 'LLL:EXT:static_info_tables/locallang_db.xml:static_markets.institution_description',
+			'config' => Array (
+				'type' => 'input',	
+				'size' => '48',	
+				'max' => '90',	
+				'eval' => 'trim',
+			)
+		),
+		'acronym' => Array (		
+			'exclude' => 1,		
+			'label' => 'LLL:EXT:static_info_tables/locallang_db.xml:static_markets.acronym',
+			'config' => Array (
+				'type' => 'input',	
+				'size' => '30',	
+				'max' => '30',	
+				'eval' => 'trim',
+			)
+		),
+		'city' => Array (		
+			'exclude' => 1,		
+			'label' => 'LLL:EXT:static_info_tables/locallang_db.xml:static_markets.city',
+			'config' => Array (
+				'type' => 'input',	
+				'size' => '30',	
+				'max' => '30',	
+				'eval' => 'trim',
+			)
+		),
+		'url' => Array (		
+			'exclude' => 1,		
+			'label' => 'LLL:EXT:static_info_tables/locallang_db.xml:static_markets.url',
+			'config' => Array (
+				'type' => 'input',	
+				'size' => '48',	
+				'max' => '90',	
+				'eval' => 'trim',
+			)
+		),
+	),
+	'types' => array (
+		'0' => array('showitem' => 'country;;;;1-1-1, mic, institution_description, acronym, city, url')
+	),
+);
+
 ?>
