4d231f7d11c4209176d82effe57b3fa85c0edeba
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-04-03 06:03:58 +0000

    version 2.0.2 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables@5270 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "static_info_tables"
 #
-# Auto generated 19-12-2006 12:26
+# Auto generated 15-01-2007 15:57
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Some database tables with usefull informations about countries, languages and currencies. Still lacks some data but the overall concept is stable. Install the extension div version 0.0.5 before you make an update!',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '2.0.1',
+	'version' => '2.0.2',
 	'dependencies' => 'div',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"3ef6";s:20:"class.ext_update.php";s:4:"b79e";s:33:"class.tx_staticinfotables_div.php";s:4:"365b";s:38:"class.tx_staticinfotables_encoding.php";s:4:"0361";s:41:"class.tx_staticinfotables_syslanguage.php";s:4:"4c23";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"6e61";s:14:"ext_tables.php";s:4:"f1d8";s:14:"ext_tables.sql";s:4:"1c58";s:25:"ext_tables_static+adt.sql";s:4:"431d";s:25:"icon_static_countries.gif";s:4:"2a46";s:26:"icon_static_currencies.gif";s:4:"a1e2";s:25:"icon_static_languages.gif";s:4:"639f";s:27:"icon_static_territories.gif";s:4:"aab5";s:16:"locallang_db.xml";s:4:"0063";s:7:"tca.php";s:4:"7d88";s:14:"doc/manual.sxw";s:4:"4687";s:37:"pi1/class.tx_staticinfotables_pi1.php";s:4:"9ea3";s:39:"static/static_info_tables/constants.txt";s:4:"aaf6";s:35:"static/static_info_tables/setup.txt";s:4:"82b7";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"b593";s:20:"class.ext_update.php";s:4:"b79e";s:33:"class.tx_staticinfotables_div.php";s:4:"724a";s:38:"class.tx_staticinfotables_encoding.php";s:4:"0361";s:41:"class.tx_staticinfotables_syslanguage.php";s:4:"4c23";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"6e61";s:14:"ext_tables.php";s:4:"f1d8";s:14:"ext_tables.sql";s:4:"1c58";s:25:"ext_tables_static+adt.sql";s:4:"431d";s:25:"icon_static_countries.gif";s:4:"2a46";s:26:"icon_static_currencies.gif";s:4:"a1e2";s:25:"icon_static_languages.gif";s:4:"639f";s:27:"icon_static_territories.gif";s:4:"aab5";s:16:"locallang_db.xml";s:4:"0063";s:7:"tca.php";s:4:"7d88";s:14:"doc/manual.sxw";s:4:"4687";s:37:"pi1/class.tx_staticinfotables_pi1.php";s:4:"1064";s:39:"static/static_info_tables/constants.txt";s:4:"aaf6";s:35:"static/static_info_tables/setup.txt";s:4:"82b7";}',
 	'suggests' => array(
 	),
 );
