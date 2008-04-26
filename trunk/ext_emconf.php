efec37091a6268d567829f6587ab1ca7b6dc7358
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-26 16:53:56 +0000

    move static_taxes into the extension static_info_tables_taxes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables@9020 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -43,8 +43,6 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 	),
 	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"6e32";s:20:"class.ext_update.php";s:4:"2803";s:33:"class.tx_staticinfotables_div.php";s:4:"6bad";s:38:"class.tx_staticinfotables_encoding.php";s:4:"03fd";s:41:"class.tx_staticinfotables_syslanguage.php";s:4:"4f57";s:16:"contributors.txt";s:4:"9c5b";s:21:"ext_conf_template.txt";s:4:"5f7b";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"b526";s:14:"ext_tables.php";s:4:"7247";s:14:"ext_tables.sql";s:4:"1edc";s:25:"ext_tables_static+adt.sql";s:4:"6eaa";s:25:"icon_static_countries.gif";s:4:"2a46";s:26:"icon_static_currencies.gif";s:4:"a1e2";s:25:"icon_static_languages.gif";s:4:"639f";s:23:"icon_static_markets.gif";s:4:"bf06";s:27:"icon_static_territories.gif";s:4:"aab5";s:16:"locallang_db.xml";s:4:"33a2";s:7:"tca.php";s:4:"cadc";s:14:"doc/manual.sxw";s:4:"2769";s:37:"pi1/class.tx_staticinfotables_pi1.php";s:4:"01fc";s:39:"static/static_info_tables/constants.txt";s:4:"169d";s:35:"static/static_info_tables/setup.txt";s:4:"82b7";}',
-	'suggests' => array(
-	),
 );
 
 ?>
--- trunk/ext_tables.sql
+++ trunk/ext_tables.sql
@@ -99,31 +99,6 @@ CREATE TABLE static_languages (
   KEY parent (pid)
 );
 
-#
-# Table structure for table "static_taxes"
-#
-CREATE TABLE static_taxes (
-  uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-  pid int(11) unsigned DEFAULT '0' NOT NULL,
-  crdate int(11) unsigned DEFAULT '0' NOT NULL,
-  deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-  hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-  starttime int(11) unsigned DEFAULT '0' NOT NULL,
-  endtime int(11) unsigned DEFAULT '0' NOT NULL,
-  tx_country_iso_nr int(11) unsigned DEFAULT '0' NOT NULL,
-  tx_country_iso_2 char(2) DEFAULT '' NOT NULL,
-  tx_country_iso_3 char(3) DEFAULT '' NOT NULL,
-  tx_zn_code varchar(45) DEFAULT '' NOT NULL,
-  tx_name_en varchar(255) DEFAULT '' NOT NULL,
-  tx_scope tinyint(3) unsigned DEFAULT '0' NOT NULL,
-  tx_code varchar(5) DEFAULT '' NOT NULL,
-  tx_class tinyint(3) unsigned DEFAULT '0' NOT NULL,
-  tx_rate varchar(20) DEFAULT '' NOT NULL,
-  tx_priority tinyint(3) unsigned DEFAULT '0' NOT NULL,
-  PRIMARY KEY (uid),
-  KEY parent (pid)
-);
-
 
 #
 # Table structure for table "static_markets"
