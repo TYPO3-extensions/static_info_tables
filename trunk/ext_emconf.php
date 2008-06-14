979114b402750f23254df66c6ea51fd245bb21fe
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-14 08:45:32 +0000

    version 2.0.9 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables@9326 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "static_info_tables"
 #
-# Auto generated 02-05-2008 21:26
+# Auto generated 14-06-2008 10:42
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'misc',
 	'shy' => 0,
 	'dependencies' => '',
-	'version' => '2.0.8',
+	'version' => '2.0.9',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"db85";s:20:"class.ext_update.php";s:4:"2803";s:33:"class.tx_staticinfotables_div.php";s:4:"20ea";s:38:"class.tx_staticinfotables_encoding.php";s:4:"03fd";s:41:"class.tx_staticinfotables_syslanguage.php";s:4:"4f57";s:16:"contributors.txt";s:4:"9c5b";s:21:"ext_conf_template.txt";s:4:"5f7b";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"697d";s:14:"ext_tables.php";s:4:"dfeb";s:14:"ext_tables.sql";s:4:"be38";s:25:"ext_tables_static+adt.sql";s:4:"3d46";s:25:"icon_static_countries.gif";s:4:"2a46";s:26:"icon_static_currencies.gif";s:4:"a1e2";s:25:"icon_static_languages.gif";s:4:"639f";s:23:"icon_static_markets.gif";s:4:"bf06";s:27:"icon_static_territories.gif";s:4:"aab5";s:16:"locallang_db.xml";s:4:"158c";s:7:"tca.php";s:4:"3a72";s:14:"doc/manual.sxw";s:4:"79f9";s:37:"pi1/class.tx_staticinfotables_pi1.php";s:4:"afd4";s:39:"static/static_info_tables/constants.txt";s:4:"169d";s:35:"static/static_info_tables/setup.txt";s:4:"82b7";}',
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"8a80";s:20:"class.ext_update.php";s:4:"2803";s:33:"class.tx_staticinfotables_div.php";s:4:"d704";s:38:"class.tx_staticinfotables_encoding.php";s:4:"03fd";s:41:"class.tx_staticinfotables_syslanguage.php";s:4:"4f57";s:16:"contributors.txt";s:4:"9c5b";s:21:"ext_conf_template.txt";s:4:"5f7b";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"5951";s:14:"ext_tables.php";s:4:"dfeb";s:14:"ext_tables.sql";s:4:"be38";s:25:"ext_tables_static+adt.sql";s:4:"aac1";s:25:"icon_static_countries.gif";s:4:"2a46";s:26:"icon_static_currencies.gif";s:4:"a1e2";s:25:"icon_static_languages.gif";s:4:"639f";s:23:"icon_static_markets.gif";s:4:"bf06";s:27:"icon_static_territories.gif";s:4:"aab5";s:16:"locallang_db.xml";s:4:"158c";s:7:"tca.php";s:4:"3a72";s:14:"doc/manual.sxw";s:4:"ce72";s:37:"pi1/class.tx_staticinfotables_pi1.php";s:4:"d40f";s:39:"static/static_info_tables/constants.txt";s:4:"169d";s:35:"static/static_info_tables/setup.txt";s:4:"82b7";}',
 	'suggests' => array(
 	),
 );
