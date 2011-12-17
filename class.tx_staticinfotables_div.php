182f231838cedc25ce91ef7bd6b9d228d04f3cbe
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-17 01:13:30 +0000

    Fixed issue #28483: tx_staticinfotables_div::getCurrentLanguage issues repetitive SQL queries (Thanks to Raphaël Riel)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@55590 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_staticinfotables_div.php
+++ class.tx_staticinfotables_div.php
@@ -56,6 +56,7 @@
  */
 class tx_staticinfotables_div {
 
+	private static $cache = array();
 
 	/**
 	 * Returns a label field for the current language
@@ -183,34 +184,44 @@ class tx_staticinfotables_div {
 	 * @return	string		'DE', 'EN', 'DK', ...
 	 */
 	function getCurrentLanguage () {
-		global $LANG, $TSFE, $TYPO3_DB;
 
- 		if (is_object($TSFE)) {
-			$langCodeT3 = $TSFE->lang;
-			$csConvObj = $TSFE->csConvObj;
- 		} elseif (is_object($LANG)) {
- 			$langCodeT3 = $LANG->lang;
- 			$csConvObj = $LANG->csConvObj;
+ 		if (is_object($GLOBALS['TSFE'])) {
+			$langCodeT3 = $GLOBALS['TSFE']->lang;
+			$csConvObj = $GLOBALS['TSFE']->csConvObj;
+ 		} elseif (is_object($GLOBALS['LANG'])) {
+ 			$langCodeT3 = $GLOBALS['LANG']->lang;
+ 			$csConvObj = $GLOBALS['LANG']->csConvObj;
 		} else {
 			return 'EN';
 		}
-		
 		if ($langCodeT3 === 'default') {
 			return 'EN';
 		}
+			// Return cached value if any
+		if (isset(self::$cache['getCurrentLanguage'][$langCodeT3])) {	
+			return self::$cache['getCurrentLanguage'][$langCodeT3];
+		}
 
-		$res = $TYPO3_DB->exec_SELECTquery(
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
 			'lg_iso_2,lg_country_iso_2',
 			'static_languages',
-			'lg_typo3=' . $TYPO3_DB->fullQuoteStr($langCodeT3, 'static_languages')
+			'lg_typo3=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($langCodeT3, 'static_languages')
 		);
-		while ($row = $TYPO3_DB->sql_fetch_assoc($res)) {
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
 			$lang = $row['lg_iso_2'] . ($row['lg_country_iso_2'] ? '_' . $row['lg_country_iso_2'] : '');
 		}
-		$TYPO3_DB->sql_free_result($res);
+		$GLOBALS['TYPO3_DB']->sql_free_result($res);
 
-		$rc = $lang ? $lang : $csConvObj->conv_case('utf-8', $langCodeT3, 'toUpper');
-		return $rc;
+		$lang = $lang ? $lang : $csConvObj->conv_case('utf-8', $langCodeT3, 'toUpper');
+
+			// Initialize cache array
+		if (!is_array(self::$cache['getCurrentLanguage'])) {
+			self::$cache['getCurrentLanguage'] = array();
+		}
+			// Cache retrieved value
+		self::$cache['getCurrentLanguage'][$langCodeT3] = $lang;
+
+		return $lang;
 	}
 
 
