728d1d1a97dbe7dec578a053812417d07dd08f3e
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-21 23:50:26 +0000

    Resolves #45733: Move icons to Resources directory
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables@71403 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_tables.php
+++ trunk/ext_tables.php
@@ -17,7 +17,7 @@ $GLOBALS['TCA']['static_territories'] = array(
 		'default_sortby' => 'ORDER BY tr_name_en',
 		'title' => 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_db.xlf:static_territories.title',
 		'dynamicConfigFile' => PATH_BE_staticinfotables . 'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel . 'icon_static_territories.gif',
+		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_territories.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'tr_name_en,tr_iso_nr'
@@ -40,7 +40,7 @@ $GLOBALS['TCA']['static_countries'] = array(
 		'delete' => 'deleted',
 		'title' => 'LLL:EXT:'.STATIC_INFO_TABLES_EXTkey.'/Resources/Private/Language/locallang_db.xlf:static_countries.title',
 		'dynamicConfigFile' => PATH_BE_staticinfotables.'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel.'icon_static_countries.gif',
+		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_countries.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'cn_iso_2,cn_iso_3,cn_iso_nr,cn_official_name_local,cn_official_name_en,cn_capital,cn_tldomain,cn_currency_iso_3,cn_currency_iso_nr,cn_phone,cn_uno_member,cn_eu_member,cn_address_format,cn_short_en'
@@ -59,7 +59,7 @@ $GLOBALS['TCA']['static_country_zones'] = array(
 		'default_sortby' => 'ORDER BY zn_name_local',
 		'title' => 'LLL:EXT:'.STATIC_INFO_TABLES_EXTkey.'/Resources/Private/Language/locallang_db.xlf:static_country_zones.title',
 		'dynamicConfigFile' => PATH_BE_staticinfotables.'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel.'icon_static_countries.gif',
+		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_countries.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'zn_country_iso_nr,zn_country_iso_3,zn_code,zn_name_local,zn_name_en'
@@ -78,7 +78,7 @@ $GLOBALS['TCA']['static_languages'] = array(
 		'default_sortby' => 'ORDER BY lg_name_en',
 		'title' => 'LLL:EXT:'.STATIC_INFO_TABLES_EXTkey.'/Resources/Private/Language/locallang_db.xlf:static_languages.title',
 		'dynamicConfigFile' => PATH_BE_staticinfotables.'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel.'icon_static_languages.gif',
+		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_languages.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'lg_name_local,lg_name_en,lg_iso_2,lg_typo3,lg_country_iso_2,lg_collate_locale,lg_sacred,lg_constructed'
@@ -97,7 +97,7 @@ $GLOBALS['TCA']['static_currencies'] = array(
 		'default_sortby' => 'ORDER BY cu_name_en',
 		'title' => 'LLL:EXT:'.STATIC_INFO_TABLES_EXTkey.'/Resources/Private/Language/locallang_db.xlf:static_currencies.title',
 		'dynamicConfigFile' => PATH_BE_staticinfotables.'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel.'icon_static_currencies.gif',
+		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_currencies.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'cu_iso_3,cu_iso_nr,cu_name_en,cu_symbol_left,cu_symbol_right,cu_thousands_point,cu_decimal_point,cu_decimal_digits,cu_sub_name_en,cu_sub_divisor,cu_sub_symbol_left,cu_sub_symbol_right'
