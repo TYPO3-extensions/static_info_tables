22ad3882687ea5e73291ee8526e3684282b000f3
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-26 04:53:57 +0000

    Resolves #45847: Move TCA arrays to Configuration directory
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables@71586 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_tables.php
+++ trunk/ext_tables.php
@@ -6,26 +6,9 @@ if (!defined ('TYPO3_MODE')) {
 // Configure extension static template
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('static_info_tables', 'Configuration/TypoScript/', 'Static Info Tables');
 
-$GLOBALS['TCA']['static_territories'] = array(
-	'ctrl' => array(
-		'label' => 'tr_name_en',
-		'label_alt' => 'tr_iso_nr',
-		'label_alt_force' => 1,
-		'label_userFunc' => 'SJBR\StaticInfoTables\Hook\Backend\Form\ElementRenderingHelper->addIsoCodeToLabel',
-		'readOnly' => 1,
-		'adminOnly' => 1,
-		'rootLevel' => 1,
-		'is_static' => 1,
-		'default_sortby' => 'ORDER BY tr_name_en',
-		'delete' => 'deleted',
-		'title' => 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_db.xlf:static_territories.title',
-		'dynamicConfigFile' => PATH_BE_staticinfotables . 'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_territories.gif',
-	),
-	'interface' => array(
-		'showRecordFieldList' => 'tr_name_en,tr_iso_nr'
-	)
-);
+$extensionResourcesLanguagePath = 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_db.xlf:';
+$extensionConfigurationTcaPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('static_info_tables') . 'Configuration/Tca/';
+$extensionResourcesIconsPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('static_info_tables') . 'Resources/Public/Images/Icons/';
 
 // Country reference data from ISO 3166-1
 $GLOBALS['TCA']['static_countries'] = array(
@@ -34,15 +17,14 @@ $GLOBALS['TCA']['static_countries'] = array(
 		'label_alt' => 'cn_iso_2',
 		'label_alt_force' => 1,
 		'label_userFunc' => 'SJBR\StaticInfoTables\Hook\Backend\Form\ElementRenderingHelper->addIsoCodeToLabel',
-		'readOnly' => 1,
 		'adminOnly' => 1,
 		'rootLevel' => 1,
 		'is_static' => 1,
 		'default_sortby' => 'ORDER BY cn_short_en',
 		'delete' => 'deleted',
-		'title' => 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_db.xlf:static_countries.title',
-		'dynamicConfigFile' => PATH_BE_staticinfotables . 'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_countries.gif',
+		'title' => $extensionResourcesLanguagePath . 'static_countries.title',
+		'dynamicConfigFile' => $extensionConfigurationTcaPath . 'Country.php',
+		'iconfile' => $extensionResourcesIconsPath . 'icon_static_countries.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'cn_iso_2,cn_iso_3,cn_iso_nr,cn_official_name_local,cn_official_name_en,cn_capital,cn_tldomain,cn_currency_iso_3,cn_currency_iso_nr,cn_phone,cn_uno_member,cn_eu_member,cn_address_format,cn_short_en'
@@ -54,40 +36,17 @@ $GLOBALS['TCA']['static_country_zones'] = array(
 	'ctrl' => array(
 		'label' => 'zn_name_local',
 		'label_alt' => 'zn_name_local,zn_code',
-		'readOnly' => 1,
 		'adminOnly' => 1,
 		'rootLevel' => 1,
 		'is_static' => 1,
 		'default_sortby' => 'ORDER BY zn_name_local',
 		'delete' => 'deleted',
-		'title' => 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_db.xlf:static_country_zones.title',
-		'dynamicConfigFile' => PATH_BE_staticinfotables . 'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_countries.gif',
+		'title' => $extensionResourcesLanguagePath . 'static_country_zones.title',
+		'dynamicConfigFile' => $extensionConfigurationTcaPath . 'CountryZone.php',
+		'iconfile' => $extensionResourcesIconsPath . 'icon_static_countries.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'zn_country_iso_nr,zn_country_iso_3,zn_code,zn_name_local,zn_name_en'
-	)
-);
-
-// Language reference data from ISO 639-1
-$GLOBALS['TCA']['static_languages'] = array(
-	'ctrl' => array(
-		'label' => 'lg_name_en',
-		'label_alt' => 'lg_iso_2',
-		'label_alt_force' => 1,
-		'label_userFunc' => 'SJBR\StaticInfoTables\Hook\Backend\Form\ElementRenderingHelper->addIsoCodeToLabel',
-		'readOnly' => 1,
-		'adminOnly' => 1,
-		'rootLevel' => 1,
-		'is_static' => 1,
-		'default_sortby' => 'ORDER BY lg_name_en',
-		'delete' => 'deleted',
-		'title' => 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_db.xlf:static_languages.title',
-		'dynamicConfigFile' => PATH_BE_staticinfotables . 'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_languages.gif',
-	),
-	'interface' => array(
-		'showRecordFieldList' => 'lg_name_local,lg_name_en,lg_iso_2,lg_typo3,lg_country_iso_2,lg_collate_locale,lg_sacred,lg_constructed'
 	)
 );
 
@@ -98,26 +57,65 @@ $GLOBALS['TCA']['static_currencies'] = array(
 		'label_alt' => 'cu_iso_3',
 		'label_alt_force' => 1,
 		'label_userFunc' => 'SJBR\StaticInfoTables\Hook\Backend\Form\ElementRenderingHelper->addIsoCodeToLabel',
-		'readOnly' => 1,
 		'adminOnly' => 1,
 		'rootLevel' => 1,
 		'is_static' => 1,
 		'default_sortby' => 'ORDER BY cu_name_en',
 		'delete' => 'deleted',
-		'title' => 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_db.xlf:static_currencies.title',
-		'dynamicConfigFile' => PATH_BE_staticinfotables . 'tca.php',
-		'iconfile' => PATH_BE_staticinfotables_rel . 'Resources/Public/Images/Icons/icon_static_currencies.gif',
+		'title' => $extensionResourcesLanguagePath . 'static_currencies.title',
+		'dynamicConfigFile' => $extensionConfigurationTcaPath . 'Currency.php',
+		'iconfile' => $extensionResourcesIconsPath . 'icon_static_currencies.gif',
 	),
 	'interface' => array(
 		'showRecordFieldList' => 'cu_iso_3,cu_iso_nr,cu_name_en,cu_symbol_left,cu_symbol_right,cu_thousands_point,cu_decimal_point,cu_decimal_digits,cu_sub_name_en,cu_sub_divisor,cu_sub_symbol_left,cu_sub_symbol_right'
 	)
 );
 
-$GLOBALS['TCA']['static_countries']['ctrl']['readOnly'] = 0;
-$GLOBALS['TCA']['static_languages']['ctrl']['readOnly'] = 0;
-$GLOBALS['TCA']['static_country_zones']['ctrl']['readOnly'] = 0;
-$GLOBALS['TCA']['static_currencies']['ctrl']['readOnly'] = 0;
-$GLOBALS['TCA']['static_territories']['ctrl']['readOnly'] = 0;
+// Language reference data from ISO 639-1
+$GLOBALS['TCA']['static_languages'] = array(
+	'ctrl' => array(
+		'label' => 'lg_name_en',
+		'label_alt' => 'lg_iso_2',
+		'label_alt_force' => 1,
+		'label_userFunc' => 'SJBR\StaticInfoTables\Hook\Backend\Form\ElementRenderingHelper->addIsoCodeToLabel',
+		'adminOnly' => 1,
+		'rootLevel' => 1,
+		'is_static' => 1,
+		'default_sortby' => 'ORDER BY lg_name_en',
+		'delete' => 'deleted',
+		'title' => $extensionResourcesLanguagePath . 'static_languages.title',
+		'dynamicConfigFile' => $extensionConfigurationTcaPath . 'Language.php',
+		'iconfile' => $extensionResourcesIconsPath . 'icon_static_languages.gif',
+	),
+	'interface' => array(
+		'showRecordFieldList' => 'lg_name_local,lg_name_en,lg_iso_2,lg_typo3,lg_country_iso_2,lg_collate_locale,lg_sacred,lg_constructed'
+	)
+);
+
+// UN Territory reference data 
+$GLOBALS['TCA']['static_territories'] = array(
+	'ctrl' => array(
+		'label' => 'tr_name_en',
+		'label_alt' => 'tr_iso_nr',
+		'label_alt_force' => 1,
+		'label_userFunc' => 'SJBR\StaticInfoTables\Hook\Backend\Form\ElementRenderingHelper->addIsoCodeToLabel',
+		'adminOnly' => 1,
+		'rootLevel' => 1,
+		'is_static' => 1,
+		'default_sortby' => 'ORDER BY tr_name_en',
+		'delete' => 'deleted',
+		'title' => $extensionResourcesLanguagePath . 'static_territories.title',
+		'dynamicConfigFile' => $extensionConfigurationTcaPath . 'Territory.php',
+		'iconfile' => $extensionResourcesIconsPath . 'icon_static_territories.gif',
+	),
+	'interface' => array(
+		'showRecordFieldList' => 'tr_name_en,tr_iso_nr'
+	)
+);
+
+unset($extensionResourcesLanguagePath);
+unset($extensionConfigurationTcaPath);
+unset($extensionResourcesIconsPath);
 
 // Configure static language field of sys_language table
 \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('sys_language');
