a4fbfe5c70ef96113607efb8f4c05c5d62ce3aa2
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-26 04:59:56 +0000

    Resolves #45848: Add domain model
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@71587 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,11 +1,15 @@
 <?php
 /*
  * Register necessary class names with autoloader
- *
  */
 $extensionClassesPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('static_info_tables') . 'Classes/';
 return array(
 	'SJBR\StaticInfoTables\PiBaseApi' => $extensionClassesPath . 'PiBaseApi.php',
+	'SJBR\StaticInfoTables\Domain\Model\Country' => $extensionClassesPath . 'Domain/Model/Country.php',
+	'SJBR\StaticInfoTables\Domain\Model\CountryZone' => $extensionClassesPath . 'Domain/Model/CountryZone.php',
+	'SJBR\StaticInfoTables\Domain\Model\Currency' => $extensionClassesPath . 'Domain/Model/Currency.php',
+	'SJBR\StaticInfoTables\Domain\Model\Language' => $extensionClassesPath . 'Domain/Model/Language.php',
+	'SJBR\StaticInfoTables\Domain\Model\Territory' => $extensionClassesPath . 'Domain/Model/Territory.php',
 	'SJBR\StaticInfoTables\Hook\Backend\Form\ElementRenderingHelper' => $extensionClassesPath . 'Hook/Backend/Form/ElementRenderingHelper.php',
 	'SJBR\StaticInfoTables\Hook\Backend\Form\SuggestReceiver' => $extensionClassesPath . 'Hook/Backend/Form/SuggestReceiver.php',
 	'SJBR\StaticInfoTables\Hook\Core\DataHandling\ProcessDataMap' => $extensionClassesPath . 'Hook/Core/DataHandling/ProcessDataMap.php',
