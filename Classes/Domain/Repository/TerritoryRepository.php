01a6c58579bdf9a55e8866e98523822998e377ce
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-03 16:20:36 +0000

    Resolves #45986: Add domain repositories classes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@72109 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Domain/Repository/TerritoryRepository.php
@@ -0,0 +1,49 @@
+<?php
+namespace SJBR\StaticInfoTables\Domain\Repository;
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011-2012 Armin Rüdiger Vieweg <info@professorweb.de>
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Repository for \SJBR\StaticInfoTables\Domain\Model\Territory
+ *
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class TerritoryRepository extends AbstractEntityRepository {
+
+	/**
+	 * Finds territories by country
+	 *
+	 * @param \SJBR\StaticInfoTables\Domain\Model\Country $country
+	 *
+	 * @return \TYPO3\CMS\Extbase\Persistence\QueryResultInterface|array
+	 */
+	public function findByCountry(\SJBR\StaticInfoTables\Domain\Model\Country $country) {
+		$query = $this->createQuery();
+		$query->matching(
+			$query->equals('tr_iso_nr', $country->getParentTerritoryCode())
+		);
+		return $query->execute();
+	}
+}
+?>
