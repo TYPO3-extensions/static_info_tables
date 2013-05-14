d13498e07877dc2ee7c6336de73da500b5308b42
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-14 04:41:10 +0000

    Update version to 6.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@75344 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Data and API for countries, languages and currencies.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '3.0.0',
+	'version' => '6.0.0',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
@@ -25,19 +25,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearcacheonload' => 1,
 	'lockType' => '',
-	'author' => 'René Fritz',
-	'author_email' => 'r.fritz@colorcube.de',
-	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
+	'author' => 'Stanislas Rolland/René Fritz',
+	'author_email' => 'typo3@sjbr.ca',
+	'author_company' => 'SJBR',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.6-0.0.0',
+			'typo3' => '6.0.6-6.2.99',
 			'php' => '5.3.7-0.0.0',
 		),
 		'conflicts' => array(
 			'sr_static_info' => '',
 			'cc_infotablesmgm' => '',
+			'static_info_tables_extbase' => '',
 		),
 		'suggests' => array(
 		),
