0440832aa65cd0066878422d8f5b976e18bca6e2
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-24 23:36:24 +0000

    Update ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@75521 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "static_info_tables".
-#
-# Auto generated 16-12-2011 20:46
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables".
+ *
+ * Auto generated 24-05-2013 16:38
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Static Info Tables',
 	'description' => 'Data and API for countries, languages and currencies.',
 	'category' => 'misc',
@@ -25,24 +25,28 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearcacheonload' => 1,
 	'lockType' => '',
-	'author' => 'Stanislas Rolland/René Fritz',
+	'author' => 'Stanislas Rolland/Rene Fritz',
 	'author_email' => 'typo3@sjbr.ca',
 	'author_company' => 'SJBR',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'typo3' => '6.0.6-6.1.99',
 			'php' => '5.3.7-0.0.0',
 		),
-		'conflicts' => array(
-			'sr_static_info' => '',
-			'cc_infotablesmgm' => '',
+		'conflicts' => 
+		array (
+			'sr_static_info' => '0.0.0-99.99.99',
+			'cc_infotablesmgm' => '0.0.0-99.99.99',
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
-	'_md5_values_when_last_written' => ''
+	'_md5_values_when_last_written' => '',
 );
 
 ?>
