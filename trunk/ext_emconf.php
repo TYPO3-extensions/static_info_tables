07d42a188e44f7e253f6a3ee6d664347092f0257
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-25 16:54:40 +0000

    Update version to 6.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables@75529 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "static_info_tables".
  *
- * Auto generated 24-05-2013 16:38
+ * Auto generated 25-05-2013 12:52
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'description' => 'Data and API for countries, languages and currencies.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '6.0.0',
+	'version' => '6.0.1',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
