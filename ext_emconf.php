f31e01b13ddccf730bb83455c74a0b14926b88c9
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-02-10 05:29:04 +0000

    Enable in TYPO3 CMS 7.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables/trunk@88349 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'typo3' => '6.2.0-7.0.99',
+			'typo3' => '6.2.0-7.1.99',
 			'php' => '5.3.7-0.0.0',
 		),
 		'conflicts' => 
