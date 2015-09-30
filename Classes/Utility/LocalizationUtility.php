91c5ea872fe3c0b36578da2afc0d9dd4d04a5edd
Stanislas Rolland <typo3@sjbr.ca>
2015-09-30 18:44:12 -0400

    [BUGFIX] Remove reference to removed method
    Resolves: #70241
    Change-Id: I4989bd1f885f57c09edca953590e411439fda793
    Reviewed-on: http://review.typo3.org/43676
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- Classes/Utility/LocalizationUtility.php
+++ Classes/Utility/LocalizationUtility.php
@@ -133,9 +133,6 @@ class LocalizationUtility {
 	 * @return array field names
 	 */
 	public static function getLabelFields ($tableName, $lang, $local = FALSE) {
-		if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 6001000) {
-			TcaUtility::loadTca($tableName);
-		}
 		$labelFields = array();
 		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['static_info_tables']['tables'][$tableName]['label_fields'])) {
 			$alternativeLanguages = array();
@@ -312,4 +309,3 @@ class LocalizationUtility {
 		}
 	}
 }
-?>
