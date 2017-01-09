b7496ff97a77f35ffa931a3e8797b976953b4e4e
Stanislas Rolland <typo3@sjbr.ca>
2017-01-09 14:09:17 -0500

    [FEATURE] Missing extbase declaration for field "static_lang_isocode"
    Adding domain model and repository for system language
    
    Change-Id: Idff2662c669a5a1be45a5ec61d679a762fc64dab
    Resolves: #77246

---
+++ Classes/Domain/Model/SystemLanguage.php
@@ -0,0 +1,91 @@
+<?php
+namespace SJBR\StaticInfoTables\Domain\Model;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013-2017 Stanislas Rolland <typo3(arobas)sjbr.ca>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use SJBR\StaticInfoTables\Domain\Model\Language;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * The System Language model
+ */
+class SystemLanguage extends AbstractEntity
+{
+	/**
+	 * @var string System language name
+	 */
+	protected $title = '';
+
+	/**
+	 * @var \SJBR\StaticInfoTables\Domain\Model\Language
+	 */
+	protected $isoLanguage = null;
+
+	/**
+	 * Sets the language name
+	 *
+	 * @param string $title
+	 * @return void
+	 */
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+
+	/**
+	 * Gets the backend language name
+	 *
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	/**
+	 * Sets the ISO language
+	 *
+	 * @param Language $isoLanguage
+	 * @return void
+	 */
+	public function setIsoLanguage(Language $isoLanguage)
+	{
+		$this->isoLanguage = $isoLanguage;
+	}
+
+	/**
+	 * Gets the ISO language
+	 *
+	 * @return Language
+	 */
+	public function getIsoLanguage()
+	{
+		if ($this->isoLanguage !== null) {
+			return clone $this->isoLanguage;
+		} else {
+			return $this->isoLanguage;
+		}
+	}
+}
