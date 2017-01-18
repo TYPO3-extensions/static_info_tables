379dbd4a34c5f3888d1f292a95f81022c7585e0f
Stanislas Rolland <typo3@sjbr.ca>
2017-01-18 01:07:12 -0500

    [TASK] Follow-up to Cleanup extension configuration
    Change-Id: Ib50dab695510415512be24d70a4029c84df7bb62
    Resolves: #79207

--- ext_tables.php
+++ ext_tables.php
@@ -5,9 +5,9 @@ if (TYPO3_MODE == 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
 	/**
 	 * Registers the Static Info Tables Manager backend module, if enabled
 	 */
-	if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['enableManager']) {
+	if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['static_info_tables']['enableManager']) {
 		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-			'SJBR.' . $_EXTKEY,
+			'SJBR.static_info_tables',
 			// Make module a submodule of 'tools'
 			'tools',
 			// Submodule key
@@ -20,11 +20,11 @@ if (TYPO3_MODE == 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
 			),
 			array(
 				'access' => 'user,group',
-				'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Images/Icons/StaticInfoTablesManager.png',
-				'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xlf'
+				'icon' => 'EXT:static_info_tables/Resources/Public/Icons/Extension.svg',
+				'labels' => 'LLL:EXT:static_info_tables/Resources/Private/Language/locallang_mod.xlf'
 			)
 		);
 		// Add module configuration setup
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TypoScript/Manager/setup.txt">');
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('static_info_tables', 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:static_info_tables/Configuration/TypoScript/Manager/setup.txt">');
 	}
 }
