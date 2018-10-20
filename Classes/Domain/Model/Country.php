74050317cd249093d88848d4325727214c093435
Stanislas Rolland <typo3@sjbr.ca>
2018-10-20 17:24:20 -0400

    [TASK] Use @TYPO3\CMS\Extbase\Annotation\ORM\Lazy annotation
    @lazy annotation is deprecated in TYPO3 9 LTS.
    
    Change-Id: I981edc50e160205b45855dca62658cbbc1288522
    Resolves: #86707
    Reviewed-on: https://review.typo3.org/58668
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- Classes/Domain/Model/Country.php
+++ Classes/Domain/Model/Country.php
@@ -1,39 +1,39 @@
 <?php
 namespace SJBR\StaticInfoTables\Domain\Model;
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2011-2012 Armin Rüdiger Vieweg <info@professorweb.de>
-*  (c) 2013-2014 Stanislas Rolland <typo3(arobas)sjbr.ca>
-*
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
-/**
- * The Country model
+
+/*
+ *  Copyright notice
  *
- * @copyright Copyright belongs to the respective authors
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *  (c) 2011-2012 Armin Rüdiger Vieweg <info@professorweb.de>
+ *  (c) 2013-2018 Stanislas Rolland <typo3(arobas)sjbr.ca>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
  */
 
 use SJBR\StaticInfoTables\Utility\ModelUtility;
+use TYPO3\CMS\Extbase\Annotation\ORM\Lazy;
 
-class Country extends AbstractEntity {
+/**
+ * The Country model
+ */
+class Country extends AbstractEntity
+{
 	/**
 	 * @var string
 	 */
@@ -48,6 +48,7 @@ class Country extends AbstractEntity {
 	 * Country zones of this country
 	 *
 	 * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\SJBR\StaticInfoTables\Domain\Model\CountryZone>
+	 * @Lazy
 	 * @lazy
 	 */
 	protected $countryZones;
