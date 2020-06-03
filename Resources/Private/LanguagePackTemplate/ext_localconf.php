20c5e81ae357402db434c2fd3708d65c66b345a9
Stanislas Rolland <typo32020@sjbr.ca>
2020-06-03 13:14:28 -0400

    [FEATURE] Use Symfony expression language for TypoScript conditions
    Change-Id: I54077037468f1efe31001c28e42413029a769cc8
    Resolves: #91575
    Reviewed-on: https://review.typo3.org/c/TYPO3CMS/Extensions/static_info_tables/+/64666
    Tested-by: Stanislas Rolland <typo32020@sjbr.ca>
    Reviewed-by: Stanislas Rolland <typo32020@sjbr.ca>

--- Resources/Private/LanguagePackTemplate/ext_localconf.php
+++ Resources/Private/LanguagePackTemplate/ext_localconf.php
@@ -1,6 +1,18 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TypoScript/Extbase/setup.txt">');
-// Configure translation of suggestions labels
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTSconfig/Suggest.tsconfig">');
+call_user_func(
+    function ($extKey) {
+		// Configure translation of suggestions labels
+		if (version_compare(TYPO3_branch, '9.5', '>=')) {
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+				'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
+			);
+		} else {
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+				'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest_prior_9.tsconfig">'
+			);
+		}
+    },
+    'static_info_tables_###LANG_ISO_LOWER###'
+);
